8b51c9a486533a7a98e11179eb665a20fae80dd9
kentheckel <kent.heckel@gmail.com>
2025-05-21 15:19:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,24 +19,29 @@ try:
         now = datetime.now()
         current_minute = now.strftime("%H:%M")
 
-        # Only update screen when minute changes AND we're not in the menu
+        # Update the clock only when the minute changes
         if not menu_mode and current_minute != last_minute:
             last_minute = current_minute
             display.update_clock(hour_format=config["hour_format"], alarms=alarms.get_alarms())
 
+        # Check for dial input
         action = dial.get_input()
+        if action:
+            print("Action:", action)
 
         if action == "press":
             menu_mode = not menu_mode
             if menu_mode:
                 display.draw_menu()
             else:
+                # Reset selected index if you want to return to top each time:
+                # display.selected_index = 0
                 display.update_clock(hour_format=config["hour_format"], alarms=alarms.get_alarms())
 
         elif action in ["up", "down"] and menu_mode:
             display.update_menu_selection(action)
 
-        time.sleep(0.1)
+        time.sleep(0.05)  # faster polling for more responsive scrolling
 
 except KeyboardInterrupt:
     print("Exiting.")
