e49ae87f72cdfb335b928782ade93d65f330f5cf
kentheckel <kent.heckel@gmail.com>
2025-05-21 15:15:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from alarm import AlarmManager
 from config import load_config
 
 import time
+from datetime import datetime
 
 display = DisplayManager()
 dial = DialInput()
@@ -11,13 +12,17 @@ alarms = AlarmManager()
 config = load_config()
 
 menu_mode = False
+last_minute = None
 
 try:
     while True:
-        if not menu_mode:
-            display.update_clock(hour_format=config["hour_format"], alarms=alarms.get_alarms())
+        now = datetime.now()
+        current_minute = now.strftime("%H:%M")
 
-        time.sleep(0.5)
+        # Only update screen when minute changes AND we're not in the menu
+        if not menu_mode and current_minute != last_minute:
+            last_minute = current_minute
+            display.update_clock(hour_format=config["hour_format"], alarms=alarms.get_alarms())
 
         action = dial.get_input()
 
@@ -28,9 +33,10 @@ try:
             else:
                 display.update_clock(hour_format=config["hour_format"], alarms=alarms.get_alarms())
 
-
         elif action in ["up", "down"] and menu_mode:
             display.update_menu_selection(action)
+
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     print("Exiting.")
